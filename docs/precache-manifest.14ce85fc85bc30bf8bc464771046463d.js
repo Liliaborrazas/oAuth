cd7028592fbdd3991fde60ef35b3fc9291b73df4
Lilia Mabel Borrazás <lilia.borrazas@ultimasoftware.com>
2019-08-20 13:14:39 +0200

    create app

---
+++ docs/precache-manifest.14ce85fc85bc30bf8bc464771046463d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57055d572e3aa35e0816a46ecde515e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "973c3266f34906911838",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cc29e8f8b5f4cc74a655",
+    "url": "/static/js/2.b877ee89.chunk.js"
+  },
+  {
+    "revision": "973c3266f34906911838",
+    "url": "/static/js/main.1ee9c3f1.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  }
+]);
